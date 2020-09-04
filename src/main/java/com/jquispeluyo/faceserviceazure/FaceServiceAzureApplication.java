bee0329848c08f27a166139ae6eb2253d14c14fa
José Luis Quispe Luyo <jose.quispe@vallegrande.edu.pe>
2020-09-04 17:23:12 -0500

    first commit

---
+++ src/main/java/com/jquispeluyo/faceserviceazure/FaceServiceAzureApplication.java
@@ -0,0 +1,13 @@
+package com.jquispeluyo.faceserviceazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceServiceAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceServiceAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jquispeluyo/faceserviceazure/FaceServiceAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jquispeluyo.faceserviceazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceServiceAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
