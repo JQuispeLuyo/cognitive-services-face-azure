e46e37f49575f2f00d6fbda612457acde11ff864
José Luis Quispe Luyo <jose.quispe@vallegrande.edu.pe>
2020-09-05 18:37:45 -0500

    fix

--- api/src/main/java/com/jquispeluyo/faceserviceazure/gcs/GscServiceImpl.java
+++ api/src/main/java/com/jquispeluyo/faceserviceazure/gcs/GscServiceImpl.java
@@ -1,12 +1,10 @@
 package com.jquispeluyo.faceserviceazure.gcs;
 
 import com.google.api.client.util.DateTime;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Acl;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,15 +16,8 @@ import java.util.Date;
 @Service
 public class GscServiceImpl implements GcsService {
 
+    @Autowired
     private Storage storage;
-
-    public GscServiceImpl() throws IOException {
-        storage = StorageOptions.newBuilder()
-                .setProjectId("ci-cd-demo-275102")
-                .setCredentials(GoogleCredentials.fromStream(new ClassPathResource("static/json-key.json").getInputStream()))
-                .build()
-                .getService();
-    }
 
     @Override
     public String upload(MultipartFile filePart) throws IOException {
