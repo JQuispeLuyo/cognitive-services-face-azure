d33ad240571b8af312c7c8093b9440f7e94821a7
José Luis Quispe Luyo <jose.quispe@vallegrande.edu.pe>
2020-09-06 14:20:55 -0500

    test env

--- api/src/main/java/com/jquispeluyo/faceserviceazure/gcs/GscServiceImpl.java
+++ api/src/main/java/com/jquispeluyo/faceserviceazure/gcs/GscServiceImpl.java
@@ -1,14 +1,22 @@
 package com.jquispeluyo.faceserviceazure.gcs;
 
 import com.google.api.client.util.DateTime;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Acl;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -16,8 +24,23 @@ import java.util.Date;
 @Service
 public class GscServiceImpl implements GcsService {
 
-    @Autowired
     private Storage storage;
+
+    @Value("${GOOGLE_CREDENTIALS}")
+    private String gservicesConfig;
+
+    public GscServiceImpl() throws IOException {
+
+        System.out.println(gservicesConfig);
+        JsonObject jsonObject = JsonParser.parseString(gservicesConfig).getAsJsonObject();
+        InputStream is = new ByteArrayInputStream(jsonObject.toString().getBytes());
+
+        storage = StorageOptions.newBuilder()
+                .setProjectId("ci-cd-demo-275102")
+                .setCredentials(GoogleCredentials.fromStream(is))
+                .build()
+                .getService();
+    }
 
     @Override
     public String upload(MultipartFile filePart) throws IOException {
